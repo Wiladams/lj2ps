30d2cfae5d068f5405b0cb6b0e2ff51bf871ba5d
William Adams <william_a_adams@msn.com>
2019-09-12 19:23:10 -0700

    removing collections

--- testy/test_lua.lua
+++ testy/test_lua.lua
@@ -1,48 +1,24 @@
 package.path = "../?.lua;"..package.path
 
-local ps_common = require("lj2ps.ps_common")
-local Array = ps_common.Array
+local enum = require("lj2ps.enum")
 
-local function test_array()
-local arr = Array(3)
-print("Size: ", #arr)
-arr[0] = true
-arr[2] = true
-arr[3] = "fake"
-print("Values")
-print(arr[0])
-print(arr[1])
-print(arr[2])
-print(arr[3])
-print("Array Size, extended: ", #arr)
+local function test_enum()
+local some = enum {
+    [0]= "a",
+    "b",
+    [10] = "c",
+    "d"
+}
 
-print("Array.kind: ", arr.kind)
-end 
-
-local function test_truncate()
-    local v1 = 4.23
-    local v2 = -3.8
-
-    print("FLOOR")
-    print(math.floor(v1))
-    print(math.floor(v2))
-
-    print("CEIL")
-    print(math.ceil(v1))
-    print(math.ceil(v2))
+    print(some["a"])
+    print(some["b"])
+    print(some["c"])
+    print(some["d"])
+print("========")
+    print(some[0])
+    print(some[1])
+    print(some[10])
+    print(some[11])
 end
 
-local function test_unpack()
-    local function foo(a,b,c,d)
-        print(type(a),a)
-        print(b)
-        print(c)
-        print(d)
-    end
-
-    foo(unpack{"a","b","c","d"})
-end
-
---test_array()
---test_truncate()
-test_unpack()
+test_enum()
